e090e2011b763f48dd1ebcebad143c9b90f712e0
lalo adrian morales <laloadrianmorales@gmail.com>
2025-01-12 21:05:35 -0800

    Initial Commit

---
+++ PeterYangGetsOrganized/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PeterYangGetsOrganized
+//
+//  Created by brain on 1/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
